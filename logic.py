3f00ae67166fe03e480d10c7dd37b82971df263d
samatic237 <153364023+samatic237@users.noreply.github.com>
2025-04-27 16:21:40 +0300

    попытка сделать возможность задать промпт

--- logic.py
+++ logic.py
@@ -13,7 +13,18 @@ client = gigachat.Client(cfg.database,
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.send_message(message.chat.id, 'Привет! Напиши что-нибудь!')
-
+@bot.message_handler(commands=['prompt'])
+def handle_command(message):
+    args = message.text.split(' ', 1)
+    if len(args) > 1:
+        user_input = args[1]
+        PROMPT = gigachat.Message(user_input, "system")
+        client = gigachat.Client(cfg.database,
+                         gigachat.RawClient(cfg.gigachat), PROMPT)
+        
+        bot.reply_to(message, f"Вы ввели: {user_input}")
+    else:
+        bot.reply_to(message, "Пожалуйста, укажите аргумент после команды")
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
     user_id = message.from_user.id
